6e1081781e5e499598402e2a05b1b63dbf17d361
Smalley2001 <51380202+Smalley2001@users.noreply.github.com>
2021-11-25 23:37:03 -0500

    Create new project

---
+++ app/src/main/java/edu/msu/smalle45/emergency/HomeActivity.java
@@ -0,0 +1,14 @@
+package edu.msu.smalle45.emergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/edu/msu/smalle45/emergency/LoginActivity.java
@@ -0,0 +1,14 @@
+package edu.msu.smalle45.emergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
