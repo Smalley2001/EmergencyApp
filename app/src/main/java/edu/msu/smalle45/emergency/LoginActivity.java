814cc1217d0d24e07f4c5935b3c1a85ad9bdea21
Smalley2001 <51380202+Smalley2001@users.noreply.github.com>
2021-12-15 14:14:16 -0500

    Parse Login and Registration

--- app/src/main/java/edu/msu/smalle45/emergency/LoginActivity.java
+++ app/src/main/java/edu/msu/smalle45/emergency/LoginActivity.java
@@ -2,13 +2,101 @@ package edu.msu.smalle45.emergency;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+/**
+ * Activity for letting a user login to the app
+ * Registration source code: https://github.com/Smalley2001/Tout_le_Monde
+ */
 public class LoginActivity extends AppCompatActivity {
+
+    // User's username
+    private EditText etUsername;
+    // User's password
+    private EditText etPassword;
+    // Button to login into to Parse
+    private Button btnLogin;
+    // Button to go to Registration Activity
+    private TextView signUp;
+    // TAG used for debugging on Logcat
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // If user already logged in, send them to Main Activity
+        if (ParseUser.getCurrentUser() != null) {
+
+            goHomeActivity();
+        }
+
+        etUsername = findViewById(R.id.inputLoginUsername);
+        etPassword = findViewById(R.id.inputUserPassword);
+        btnLogin = findViewById(R.id.btnLoginButton);
+        signUp = findViewById(R.id.textViewSignUp);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goRegistrationActivity();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // If user clicks login button, login into Parse
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+
+
+    }
+
+    private void goHomeActivity() {
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void loginUser(String username, String password) {
+
+        // Navigate to the main activity if the user has signed properly
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                // Login Failed
+                if (e != null) {
+                    Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Login Success
+                // Go to Home Page
+                goHomeActivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void goRegistrationActivity() {
+        Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/msu/smalle45/emergency/RegistrationActivity.java
+++ app/src/main/java/edu/msu/smalle45/emergency/RegistrationActivity.java
@@ -2,13 +2,135 @@ package edu.msu.smalle45.emergency;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
+
+/**
+ * Activity for Registering a user to the app
+ * Registration source code: https://github.com/Smalley2001/Tout_le_Monde
+ */
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    // User's username
+    private EditText etUsername;
+    // User's password
+    private EditText etPassword;
+    // User's confirm password
+    private EditText etConfirmPassword;
+    // TextView if user already has an account
+    private TextView alreadyHaveAccount;
+    // Button to register user to Parse
+    private Button btnRegister;
+    // TAG used for debugging on Logcat
+    private static final String TAG = "RegistrationActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        etUsername = findViewById(R.id.inputRegUsername);
+        etPassword = findViewById(R.id.inputRegPassword);
+        etConfirmPassword = findViewById(R.id.inputRegConfirmPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        alreadyHaveAccount = findViewById(R.id.alreadyHaveAccount);
+
+        alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goLoginActivity();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String confirmPassword = etConfirmPassword.getText().toString();
+
+                // Validate Registration form
+                if (isEmpty(username, password, confirmPassword)) {
+                    Toast.makeText(RegistrationActivity.this, "At least one field is empty", Toast.LENGTH_SHORT).show();
+                } else if (passwordNotEqual(password, confirmPassword)) {
+                    Toast.makeText(RegistrationActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Valid Registration
+                    validRegistration();
+                }
+
+            }
+        });
     }
+
+    private void SignUpUser(String username, String password) {
+
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // User successfully signed up
+                    user.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            Log.i(TAG, "New user created");
+                        }
+                    });
+
+                    // User registered successfully, send to Home Page
+                    goHomeActivity();
+
+                } else {
+                    // Sign up failed
+                    Log.i(TAG, "Error: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    private void validRegistration() {
+        // Registration is valid
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        // Save user to parse server
+        SignUpUser(username, password);
+    }
+
+    private boolean isEmpty(String name, String pass, String confirm) {
+
+        return name.isEmpty() || pass.isEmpty() || confirm.isEmpty();
+    }
+
+    private boolean passwordNotEqual(String pass, String confirm) {
+        return !pass.equals(confirm);
+    }
+
+    private void goHomeActivity() {
+        Intent intent = new Intent(RegistrationActivity.this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    private void goLoginActivity() {
+        Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
