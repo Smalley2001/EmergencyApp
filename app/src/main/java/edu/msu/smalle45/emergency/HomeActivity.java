afceaf034133be55fcdc9c61796007e7b5cb586d
Smalley2001 <51380202+Smalley2001@users.noreply.github.com>
2021-12-15 14:17:57 -0500

    Added Home Page and updated UI

--- app/src/main/java/edu/msu/smalle45/emergency/HomeActivity.java
+++ app/src/main/java/edu/msu/smalle45/emergency/HomeActivity.java
@@ -2,13 +2,63 @@ package edu.msu.smalle45.emergency;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 public class HomeActivity extends AppCompatActivity {
+
+    // Button to create a new emergency
+    private Button btnNewEmergency;
+
+    // Button to logout of Parse
+    private Button btnLogout;
+
+    private TextView username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnNewEmergency = findViewById(R.id.btnNewEmergency);
+        btnLogout = findViewById(R.id.btnLogout);
+        username = findViewById(R.id.name);
+
+        String currUserName = ParseUser.getCurrentUser().getUsername();
+        username.setText(currUserName);
+
+        btnNewEmergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goChooseEmergencyServiceActivity();
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logout of Parse
+                ParseUser.logOut();
+                // Current user should be null
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                goWelcomeActivity();
+
+            }
+        });
+    }
+
+    private void goWelcomeActivity() {
+        Intent intent = new Intent(HomeActivity.this, WelcomeActivity.class);
+        startActivity(intent);
+    }
+
+    private void goChooseEmergencyServiceActivity() {
+        Intent intent = new Intent(HomeActivity.this, ChooseEmergencyServiceActivity.class);
+        startActivity(intent);
     }
 }
