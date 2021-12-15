235c6dbc928cf374ec2ec261e4b30c067d94547a
Smalley2001 <51380202+Smalley2001@users.noreply.github.com>
2021-12-15 14:19:35 -0500

    Create a Choose Emergency Service Activity

---
+++ app/src/main/java/edu/msu/smalle45/emergency/ChooseEmergencyServiceActivity.java
@@ -0,0 +1,63 @@
+package edu.msu.smalle45.emergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseEmergencyServiceActivity extends AppCompatActivity {
+
+    // Button to request the police
+    private Button btnPolice;
+
+    // Button to request the Fire Department
+    private Button btnFire;
+
+    // Button to request Medical/EMS Services
+    private Button btnMedical;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_emergency_service);
+
+        btnPolice = findViewById(R.id.btnPolice);
+        btnFire = findViewById(R.id.btnFire);
+        btnMedical = findViewById(R.id.btnMedical);
+
+        btnFire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // User chose to request the Fire Department
+                String emergency = "fire";
+                goGetContactsActivity(emergency);
+            }
+        });
+
+        btnMedical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // User chose to request Medical/EMS Services
+                String emergency = "medical";
+                goGetContactsActivity(emergency);
+            }
+        });
+
+        btnPolice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // User chose to request Police
+                String emergency = "police";
+                goGetContactsActivity(emergency);
+            }
+        });
+    }
+
+    private void goGetContactsActivity(String service) {
+        Intent intent = new Intent(ChooseEmergencyServiceActivity.this, GetContactsActivity.class);
+        intent.putExtra("Service", service);
+        startActivity(intent);
+    }
+}
